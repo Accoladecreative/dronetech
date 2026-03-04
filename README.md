77413ae38c747d33599a4db91d737450c1d22e7a
Kolade Oluwadare <kolade4jesus10@gmail.com>
2026-03-04 13:26:09 +0100

    chore: re-bootstrap project, added health endpoint

---
+++ README.md
@@ -0,0 +1,17 @@
+# dronetech
+
+## Requirements
+
+- Java 20
+
+## Run
+
+```bash
+./gradlew bootRun
+```
+
+## Test
+
+```bash
+./gradlew test
+```
---
+++ src/main/java/org/kolade/dronetech/DroneTechApplication.java
@@ -0,0 +1,12 @@
+package org.kolade.dronetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneTechApplication.class, args);
+    }
+}
